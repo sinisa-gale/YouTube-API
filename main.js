b3294a9c371a0cf8ee2b52be966aad43a4ca6646
Sinisa Gacic <73501410+sinisa-gale@users.noreply.github.com>
2021-03-07 20:14:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
-var selector = document.querySelector('select');
-var dogBreed = selector.value;
-var timer = setInterval(requestHttp, 5000);
+var key = 'AIzaSyAT6WSNhSQJKRzniDCqOP18xKG615o_w5k';
+var videoList = document.querySelector('.video-list');
+var search = document.querySelector('.search input');
+var searchBtn = document.querySelector('.search button');
 
-var selectChange = function(e) {
-	if (timer) {
-		clearInterval(timer);
+function getData() {
+	var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=' + search.value + '&key=' + key;
+	var req = new XMLHttpRequest();
+	req.open('GET', url);
+	req.onload = function() {
+		listVideos(JSON.parse(req.responseText));
 	}
-	dogBreed = e.target.value;
-	requestHttp();
-	timer = setInterval(requestHttp, 5000);
+	req.send();
 }
 
-var changeImage = function (imageSrc) {
-	var imgElement = document.querySelector('img');
-	if (imgElement) {
-		imgElement.setAttribute('src', imageSrc);
-	} else {
-		var imgElem = document.createElement('img');
-		imgElem.setAttribute('src', imageSrc);
-		document.querySelector('.image-container').appendChild(imgElem);
-	}
+function listVideos(arr) {
+	videoList.innerHTML = '';
+	arr.items.forEach(function(item) {
+		createVideo(item);
+	})
 }
 
-function requestHttp() {
-  var req = new XMLHttpRequest();
-  req.open("GET", "https://dog.ceo/api/breed/" + dogBreed + "/images/random");
-	req.onload = function () {
-		var imageJson= JSON.parse(req.responseText);
-		changeImage(imageJson.message);
-	}
-  req.send();
+function createVideo(source) {
+	var elementContainer = document.createElement('div');
+	var textContainer = document.createElement('div');
+	
+	var thumb = document.createElement('img');
+	thumb.setAttribute('src', source.snippet.thumbnails.default.url)
+	elementContainer.appendChild(thumb);
+
+	var title = document.createElement('h3');
+	title.textContent = source.snippet.title;
+	textContainer.appendChild(title);
+
+	var description = document.createElement('p')
+	description.textContent = source.snippet.description;
+	textContainer.appendChild(description);
+	elementContainer.appendChild(textContainer);
+
+	videoList.appendChild(elementContainer);
 }
 
-requestHttp();
-selector.addEventListener('change', selectChange);
+searchBtn.addEventListener('click', getData);
+search.addEventListener('keypress', function(e) {
+	if (e.keyCode === 13) {getData()}
+});
