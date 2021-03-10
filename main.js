3ca97168ede0e607254e1ab697e99dbe1ad88f8b
Sinisa Gacic <73501410+sinisa-gale@users.noreply.github.com>
2021-03-10 22:47:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var key = 'AIzaSyAT6WSNhSQJKRzniDCqOP18xKG615o_w5k';
 var videoList = document.querySelector('.video-list');
 var search = document.querySelector('.search input');
 var searchBtn = document.querySelector('.search button');
+var videoView = document.querySelector('iframe');
 
 function getData() {
 	var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=' + search.value + '&key=' + key;
 	var req = new XMLHttpRequest();
+	search.value = '';
 	req.open('GET', url);
 	req.onload = function() {
 		listVideos(JSON.parse(req.responseText));
@@ -20,6 +22,12 @@ function listVideos(arr) {
 	})
 }
 
+function makeElement(parent, type, attrib) {
+	var tempElement = document.createElement(type);
+	tempElement.textContent = attrib;
+	parent.appendChild(tempElement);
+}
+
 function createVideo(source) {
 	var elementContainer = document.createElement('div');
 	var textContainer = document.createElement('div');
@@ -28,15 +36,14 @@ function createVideo(source) {
 	thumb.setAttribute('src', source.snippet.thumbnails.default.url)
 	elementContainer.appendChild(thumb);
 
-	var title = document.createElement('h3');
-	title.textContent = source.snippet.title;
-	textContainer.appendChild(title);
+	makeElement(textContainer, 'h3', source.snippet.title);
+	makeElement(textContainer, 'p', source.snippet.description);
 
-	var description = document.createElement('p')
-	description.textContent = source.snippet.description;
-	textContainer.appendChild(description);
 	elementContainer.appendChild(textContainer);
-
+	elementContainer.addEventListener('click', function(){
+		videoView.setAttribute('src', 'https://www.youtube.com/embed/' + source.id.videoId);
+		videoView.classList.add('visible'); 
+	})
 	videoList.appendChild(elementContainer);
 }
 
@@ -44,3 +51,4 @@ searchBtn.addEventListener('click', getData);
 search.addEventListener('keypress', function(e) {
 	if (e.keyCode === 13) {getData()}
 });
+
